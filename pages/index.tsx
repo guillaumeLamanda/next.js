d386e6c60a4f6b7e979252a0cd1b5780480ca6ec
Guillaume Lamanda <guillaume.lamanda@gmail.com>
2020-12-29 15:04:40 +0100

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,18 @@
-import Link from 'next/link'
-import { useState } from 'react'
+import Link from "next/link";
+import { useState } from "react";
 import {
   useViewerQuery,
   useUpdateNameMutation,
   ViewerDocument,
-} from '../lib/viewer.graphql'
-import { initializeApollo } from '../lib/apollo'
+  ViewerQuery,
+  ViewerQueryVariables,
+} from "../lib/viewer.graphql";
+import { initializeApollo } from "../lib/apollo";
 
 const Index = () => {
-  const { viewer } = useViewerQuery().data!
-  const [newName, setNewName] = useState('')
-  const [updateNameMutation] = useUpdateNameMutation()
+  const { viewer } = useViewerQuery().data!;
+  const [newName, setNewName] = useState("");
+  const [updateNameMutation] = useUpdateNameMutation();
 
   const onChangeName = () => {
     updateNameMutation({
@@ -19,31 +21,30 @@ const Index = () => {
       },
       //Follow apollo suggestion to update cache
       //https://www.apollographql.com/docs/angular/features/cache-updates/#update
-      update: (
-        store,
-        {
-          data: {
-            updateName: { name },
-          },
-        }
-      ) => {
+      update: (store, { data }) => {
         // Read the data from our cache for this query.
-        const { viewer } = store.readQuery({ query: ViewerDocument })
-        const newViewer = { ...viewer }
+        const queryData = store.readQuery<ViewerQuery, ViewerQueryVariables>({
+          query: ViewerDocument,
+        });
+        if (!queryData || !data) return;
+        const newViewer = { ...queryData.viewer };
         // Add our comment from the mutation to the end.
-        newViewer.name = name
+        newViewer.name = data.updateName.name;
         // Write our data back to the cache.
-        store.writeQuery({ query: ViewerDocument, data: { viewer: newViewer } })
+        store.writeQuery({
+          query: ViewerDocument,
+          data: { viewer: newViewer },
+        });
       },
-    })
-  }
+    });
+  };
 
   return (
     <div>
-      You're signed in as {viewer.name} and you're {viewer.status}. Go to the{' '}
+      You're signed in as {viewer.name} and you're {viewer.status}. Go to the{" "}
       <Link href="/about">
         <a>about</a>
-      </Link>{' '}
+      </Link>{" "}
       page.
       <div>
         <input
@@ -54,21 +55,21 @@ const Index = () => {
         <input type="button" value="change" onClick={onChangeName} />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export async function getStaticProps() {
-  const apolloClient = initializeApollo()
+  const apolloClient = initializeApollo();
 
   await apolloClient.query({
     query: ViewerDocument,
-  })
+  });
 
   return {
     props: {
       initialApolloState: apolloClient.cache.extract(),
     },
-  }
+  };
 }
 
-export default Index
+export default Index;
